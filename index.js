f97e62a03d315173e08053d4a9a73626dbcc38e1
noah <noahdierna@me.com>
2020-05-19 09:56:26 +0200

    error

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-const countryList = require('country-list');
+
 const axios = require('axios').default;
+const countryList = require('country-list');
 
 let args = process.argv.slice(2);
 let countryCode = countryList.getCode(args[0]);
@@ -10,6 +11,7 @@ if (!countryCode) {
       console.log(error('votre nom de pays doit être en anglais '));
       process.exit(1);
 }
+
 
 let year = args[1] || new Date().getFullYear();
 
@@ -20,5 +22,5 @@ axios.get(`https://date.nager.at/api/v2/publicholidays/${year}/${countryCode}`)
       })
       .catch((error) => {
 
-            console.log(error);
+            console.log(error)("veuillez entrer le nom d'un pays en anglais et la date si possible");
       });
